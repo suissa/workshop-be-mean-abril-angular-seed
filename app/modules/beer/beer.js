eafb499ee433ecd8adf8bd43e4381d0eb8ee16e9
Suissa <jnascimento@gmail.com>
2015-05-01 20:34:25 -0300

    tomiiii

--- app/modules/beer/beer.js
+++ app/modules/beer/beer.js
@@ -1,41 +1,86 @@
-'use strict';
+(function(){
 
-angular.module('myApp.Beer', ['ngRoute'])
+  'use strict';
 
-.config(['$routeProvider', function($routeProvider) {
-  $routeProvider.when('/beers', {
-    templateUrl: 'modules/beer/views/list.html',
-    controller: 'BeerListController'
-  });
-}])
+  angular.module('myApp.Beer', ['ngRoute'])
 
-.controller('BeerListController', ['$scope', '$http', function($scope, $http) {
+  .config(['$routeProvider', function($routeProvider) {
+    $routeProvider
+      .when('/beers', {
+        templateUrl: 'modules/beer/views/list.html',
+        controller: 'BeerListController'
+      })
+      .when('/beers/:id', {
+        templateUrl: 'modules/beer/views/get.html',
+        controller: 'BeerGetController'
+      });
+  }])
 
-  var url = 'http://localhost:3000/api/beers'
-    , method = 'GET'
-    ;
+  .controller('BeerListController', BeerListController)
+  .controller('BeerGetController', BeerGetController);
 
-  $scope.title = 'Listagem das cervejas';
-  $scope.reverse = false;
-  $scope.predicate = 'name';
+  function BeerListController ($scope, $http) {
 
-  $scope.ordenar = function(predicate) {
-    $scope.predicate = predicate;
-    $scope.reverse = !$scope.reverse
-  }
+    var url = 'http://localhost:3000/api/beers'
+      , method = 'GET'
+      ;
 
-  $http({
-    url: url,
-    method: method
-  })
-  .success(function(data){
-    console.log('Data: ', data);
-    $scope.beers = data;
-    $scope.msg = 'Listagem completa.';
-  })
-  .error(function(err){
-    console.log('Erro: ', err);
-    $scope.msg = 'ERROOOOOO na listagem.';
-  });
-}]);
+    $scope.title = 'Listagem das cervejas';
+    $scope.reverse = false;
+    $scope.predicate = 'name';
+
+    $scope.ordenar = function(predicate) {
+      $scope.predicate = predicate;
+      $scope.reverse = !$scope.reverse
+    }
+
+    $http({
+      url: url,
+      method: method
+    })
+    .success(function(data){
+      console.log('Data: ', data);
+      $scope.beers = data;
+      $scope.msg = 'Listagem completa.';
+    })
+    .error(function(err){
+      console.log('Erro: ', err);
+      $scope.msg = 'ERROOOOOO na listagem.';
+    });
+  };
+
+  function BeerGetController ($scope, $http) {
+
+    var url = 'http://localhost:3000/api/beers'
+      , method = 'GET'
+      ;
+
+    $scope.title = 'Consulta de cerveja';
+
+    $http({
+      url: url,
+      method: method
+    })
+    .success(function(data){
+      console.log('Data: ', data);
+      $scope.beers = data;
+      $scope.msg = 'Listagem completa.';
+    })
+    .error(function(err){
+      console.log('Erro: ', err);
+      $scope.msg = 'ERROOOOOO na listagem.';
+    });
+  };
+
+  BeerListController.$inject = ['$scope', '$http'];
+  BeerGetController.$inject = ['$scope', '$http'];
+
+})();
+
+
+
+
+
+
+
 
